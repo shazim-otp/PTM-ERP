402277b1e5b9a855ad794363f54c872ffd63e5f3
shazim-otp <shazamotp@gmail.com>
2026-06-07 16:46:46 +0000

    test

--- auto_update.sh
+++ auto_update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo "=== $(date) ===" >> ~/NAS/PTM/deploy.log
+
 cd ~/NAS/PTM
 
-git pull origin main
+git pull origin main >> ~/NAS/PTM/deploy.log 2>&1
 
 source venv/bin/activate
 
-pip install -r requirements.txt
+pip install -r requirements.txt >> ~/NAS/PTM/deploy.log 2>&1
 
-touch app.py
+echo "Deployment finished" >> ~/NAS/PTM/deploy.log
